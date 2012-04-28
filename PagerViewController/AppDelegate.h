772932288b25c7014b23e195bfc5b1c0adedbb48
Masahiro Morodomi <morodomi at gmail.com>
2012-04-28 19:37:24 +0900

    Initial Commit

---
+++ PagerViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PagerViewController
+//
+//  Created by 政洋 諸富 on 12/04/28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
