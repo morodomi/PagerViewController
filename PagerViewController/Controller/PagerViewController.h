02cf9398f972bded6b4d1aff92024bfa323140c8
Masahiro Morodomi <morodomi at gmail.com>
2012-04-28 19:48:16 +0900

    AppDelegate changed

---
+++ PagerViewController/Controller/PagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PagerViewController.h
+//  PagerViewController
+//
+//  Created by 政洋 諸富 on 12/04/28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PagerViewController : UIViewController
+
+@end
---
+++ PagerViewController/Controller/PagerViewController.m
@@ -0,0 +1,44 @@
+//
+//  PagerViewController.m
+//  PagerViewController
+//
+//  Created by 政洋 諸富 on 12/04/28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "PagerViewController.h"
+
+@interface PagerViewController ()
+
+@end
+
+@implementation PagerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
